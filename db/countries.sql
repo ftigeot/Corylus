5594ae120978d226e44e9e9c057fb601a8935246
François Tigeot <ftigeot@wolfpond.org>
2014-01-09 17:09:50 +0100

    Database: List of countries

---
+++ db/countries.sql
@@ -0,0 +1,28 @@
+-- Corylus - ERP software
+-- Copyright (c) 2005-2014 François Tigeot
+
+-- List of countries
+
+START TRANSACTION;
+
+CREATE TABLE countries (
+	id		serial NOT NULL PRIMARY KEY,
+	name		varchar(30) NOT NULL
+);
+
+INSERT INTO countries(id,name) VALUES(1,'Belgium');
+INSERT INTO countries(id,name) VALUES(2,'France');
+INSERT INTO countries(id,name) VALUES(3,'Germany');
+INSERT INTO countries(id,name) VALUES(4,'Hong-Kong');
+INSERT INTO countries(id,name) VALUES(5,'Israel');
+INSERT INTO countries(id,name) VALUES(6,'Korea');
+INSERT INTO countries(id,name) VALUES(7,'Luxemburg');
+INSERT INTO countries(id,name) VALUES(8,'Switzerland');
+INSERT INTO countries(id,name) VALUES(9,'Togo');
+INSERT INTO countries(id,name) VALUES(10,'USA');
+INSERT INTO countries(id,name) VALUES(11,'United Kingdom');
+INSERT INTO countries(id,name) VALUES(12,'The Netherlands');
+INSERT INTO countries(id,name) VALUES(13,'Singapore');
+INSERT INTO countries(id,name) VALUES(14,'China');
+
+COMMIT;
