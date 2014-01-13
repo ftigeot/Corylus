8bd85fd13a76ba2ac2173c73ad7841eee23fcafb
François Tigeot <ftigeot@wolfpond.org>
2014-01-13 15:39:23 +0100

    Models: Fix Rails 3.2 madness

--- app/models/product_supplier.rb
+++ app/models/product_supplier.rb
@@ -4,6 +4,7 @@
 class ProductSupplier < ActiveRecord::Base
   belongs_to	:product
   belongs_to	:supplier
+  self.primary_key = :id
 
   validates_numericality_of :price, :greather_than => 0,
 	:message => "isn't a positive number"
