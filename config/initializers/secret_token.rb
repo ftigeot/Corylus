575b246991ac169e7dfc2ecd70ba79c8259eb8b2
François Tigeot <ftigeot@wolfpond.org>
2014-01-15 10:24:05 +0100

    Config: Add missing secret_token.rb (security issue)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corylus::Application.config.secret_token = '84299a2b328da7bdf9e9eda1e11ea4fd47c7de816c8c38ab6fd214ac4e3c47e6d20f2d4c48ad082a6d92104347ffeed89106a8a246adc1630dbde06ce588114a'
